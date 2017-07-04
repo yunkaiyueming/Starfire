c2c931de40eab1309da3b58e3ec2a0b40875be6a
yunkai <819664855@qq.com>
2017-07-04 14:43:40 +0800

    bugfix alert

--- cron/cron.go
+++ cron/cron.go
@@ -9,21 +9,26 @@ import (
 	"github.com/astaxie/beego/logs"
 )
 
-func CreateCronJob(d time.Duration, f func() string) {
+func CreateCronJob(d time.Duration, f func() string, fNameInfo ...string) {
 	Ticker := time.NewTicker(d)
 	runLock := false
 
+	var fName string
+	if len(fNameInfo) > 0 {
+		fName = fNameInfo[0]
+	} else {
+		pc, _, _, _ := runtime.Caller(1)
+		fName = runtime.FuncForPC(pc).Name()
+	}
+
 	for {
-		t := <-Ticker.C
+		<-Ticker.C
 		if !runLock {
 			runLock = true
 
-			pc, _, _, _ := runtime.Caller(1)
-			fName := runtime.FuncForPC(pc).Name()
-			logs.Alert(fmt.Sprintf("fname:%v,start_time:%s\n", fName, t.Format("2006-01-02 15:04:05")))
+			logs.Alert(fmt.Sprintf("fname:%v,start_time:%s\n", fName, time.Now().Format("2006-01-02 15:04:05")))
 			ret := f()
-			fmt.Println(ret)
-			logs.Alert(fmt.Sprintf("fanme:%v,end_time:%s,ret:%s\n", fName, t.Format("2006-01-02 15:04:05"), ret))
+			logs.Alert(fmt.Sprintf("fanme:%v,end_time:%s,ret:%s\n", fName, time.Now().Format("2006-01-02 15:04:05"), ret))
 
 			runLock = false
 		}
